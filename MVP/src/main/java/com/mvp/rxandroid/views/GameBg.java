d89efe4c2146bb16339b3ea923afe6c1682268a5
zhuminhua <minhua.zhu@ele.me>
2016-10-31 18:35:15 +0800

    实现动态背景

---
+++ MVP/src/main/java/com/mvp/rxandroid/views/GameBg.java
@@ -0,0 +1,66 @@
+package com.mvp.rxandroid.views;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class GameBg {
+ // 游戏背景的图片资源
+ // 为了循环播放，这里定义两个位图对象，
+ // 其资源引用的是同一张图片
+ private Bitmap bmpBackGround1;
+ private Bitmap bmpBackGround2;
+ 
+ // 游戏背景坐标
+ private int bg1x, bg1y, bg2x, bg2y;
+ 
+ private int speed = 3;
+ 
+ public GameBg(Bitmap bmpBackGround,boolean vertical) {
+  this.bmpBackGround1 = bmpBackGround;
+  this.bmpBackGround2 = bmpBackGround;
+  if (vertical) {
+   // 首先让第一张填满屏幕
+   bg1y = -Math.abs(bmpBackGround.getHeight() - GameMoveBg.screenH);
+
+   bg2y = bg1y - bmpBackGround1.getHeight() + 50;
+   bg1x = 0;
+   bg2x = 0;
+  }else {
+   bg1x = -Math.abs(bmpBackGround.getWidth() - GameMoveBg.screenW);
+   bg2x = bg1x - bmpBackGround1.getWidth() + 50;
+   bg1y = 0;
+   bg2y = 0;
+  }
+ }
+ 
+ public void draw(Canvas canvas, Paint paint){
+ 
+  canvas.drawBitmap(bmpBackGround1, bg1x, bg1y, paint);
+  canvas.drawBitmap(bmpBackGround2, bg2x, bg2y, paint);
+ }
+ public void logic(){
+  bg1y +=speed;
+  bg2y +=speed;
+ 
+  if(bg1y > GameMoveBg.screenH){
+   bg1y = bg2y - bmpBackGround1.getHeight() +50;
+  }
+  if(bg2y > GameMoveBg.screenH){
+   bg2y = bg1y - bmpBackGround1.getHeight() +50;
+  }
+ }
+
+ public void logicX(){
+  bg1x +=speed;
+  bg2x +=speed;
+
+  if(bg1x > GameMoveBg.screenW){
+   bg1x = bg2x - bmpBackGround1.getWidth() +50;
+  }
+  if(bg2x > GameMoveBg.screenW){
+   bg2x = bg1x - bmpBackGround1.getWidth() +50;
+  }
+ }
+ 
+}
---
+++ MVP/src/main/java/com/mvp/rxandroid/views/GameMoveBg.java
@@ -0,0 +1,119 @@
+package com.mvp.rxandroid.views;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.mvp.rxandroid.R;
+
+public class GameMoveBg extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+    private SurfaceHolder sfh;
+    private Paint paint;
+    private Thread th;
+    private boolean flag;
+    private Canvas canvas;
+    private Resources res = this.getResources();
+    private Bitmap bmpBackGround;// 游戏背景
+    public static int screenW;
+    public static int screenH;
+
+    private GameBg gameBg;
+
+    /**
+     * SurfaceView初始化函数
+     */
+    public GameMoveBg(Context context) {
+        super(context);
+        sfh = this.getHolder();
+        sfh.addCallback(this);
+        paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setAntiAlias(true);
+    }
+
+    public GameMoveBg(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        sfh = this.getHolder();
+        sfh.addCallback(this);
+        paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setAntiAlias(true);
+    }
+
+    /**
+     * SurfaceView视图创建，响应此函数
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        screenW = this.getWidth();
+        screenH = this.getHeight();
+        initGame();
+        flag = true;
+        // 实例线程
+        th = new Thread(this);
+        // 启动线程
+        th.start();
+    }
+
+    /**
+     * 加载游戏资源
+     */
+    private void initGame() {
+        // 加载游戏资源
+        bmpBackGround = BitmapFactory
+                .decodeResource(res, R.mipmap.login_bg_img);
+
+        gameBg = new GameBg(bmpBackGround,false);
+
+    }
+
+    @Override
+    public void run() {
+        while (flag) {
+            long start = System.currentTimeMillis();
+            try {
+                canvas = sfh.lockCanvas();
+                if (canvas != null) {
+                    canvas.drawColor(Color.WHITE);
+                    gameBg.draw(canvas, paint);
+                }
+            } catch (Exception e) {
+            } finally {
+                if (canvas != null)
+                    sfh.unlockCanvasAndPost(canvas);
+            }
+            gameBg.logicX();
+            long end = System.currentTimeMillis();
+            try {
+                if (end - start < 50) {
+                    Thread.sleep(50 - (end - start));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * SurfaceView视图状态发生改变，响应此函数
+     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+    }
+
+    /**
+     * SurfaceView视图消亡时，响应此函数
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        flag = false;
+    }
+}
