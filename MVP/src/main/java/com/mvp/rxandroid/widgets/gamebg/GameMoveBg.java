d04ae6346ce32de07526468d7ae740dc03f81b6e
zhuminhua <minhua.zhu@ele.me>
2016-11-02 17:41:20 +0800

    改背景

--- MVP/src/main/java/com/mvp/rxandroid/widgets/gamebg/GameMoveBg.java
+++ MVP/src/main/java/com/mvp/rxandroid/widgets/gamebg/GameMoveBg.java
@@ -68,7 +68,7 @@ public class GameMoveBg extends SurfaceView implements SurfaceHolder.Callback, R
     private void initGame() {
         // 加载游戏资源
         bmpBackGround = BitmapFactory
-                .decodeResource(res, R.drawable.contact_bg);
+                .decodeResource(res, R.mipmap.login_bg_img);
 
         gameBg = new GameBg(bmpBackGround,false);
 
