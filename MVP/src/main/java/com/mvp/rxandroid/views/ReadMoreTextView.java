117022c02c30fd986ced6d90f6598d6754fa6095
zhuminhua <minhua.zhu@ele.me>
2016-12-15 22:02:57 +0800

    自定义了一个可展开的textView

--- MVP/src/main/java/com/mvp/rxandroid/views/ReadMoreTextView.java
+++ MVP/src/main/java/com/mvp/rxandroid/views/ReadMoreTextView.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 czy1121
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.mvp.rxandroid.views;
 
 import android.content.Context;
@@ -31,14 +15,14 @@ import android.widget.TextView;
 
 import com.mvp.rxandroid.R;
 
-/**
- * 可展开的文本框
- */
+
 public class ReadMoreTextView extends TextView {
 
     private int mMaxLines;
     private BufferType mBufferType = BufferType.NORMAL;
     private CharSequence mText;
+    //用来整理中英文混编
+    private String fixText;
     private String mMoreText;
     private String mLessText;
     private int mMoreColor;
@@ -94,9 +78,13 @@ public class ReadMoreTextView extends TextView {
     @Override
     public void setText(final CharSequence text, BufferType type) {
         mText = text;
+        fixText = text.toString();
+        if (fixText.charAt(fixText.length() - 1) == '\n') {
+            fixText = fixText.substring(0, fixText.length() - 1);
+        }
         mBufferType = type;
         setup();
-        super.setText(text, type);
+        super.setText(fixText, type);
     }
 
     @Override
@@ -105,7 +93,7 @@ public class ReadMoreTextView extends TextView {
     }
 
     private void setup() {
-        if (mListener == null || mMaxLines < 1 || mText == null) {
+        if (mListener == null || mMaxLines < 1 || fixText == null) {
             return;
         }
         getViewTreeObserver().addOnGlobalLayoutListener(mListener);
@@ -116,7 +104,7 @@ public class ReadMoreTextView extends TextView {
         @Override
         public void onGlobalLayout() {
             getViewTreeObserver().removeOnGlobalLayoutListener(this);
-
+            fixTextWithEnter(0);
             if (getLineCount() <= mMaxLines) {
                 return;
             }
@@ -127,34 +115,46 @@ public class ReadMoreTextView extends TextView {
         }
     };
 
+    /**
+     * 给添加的字段设置颜色
+     * @param content 文本的字段
+     * @param label 新添加的字段
+     * @param color 新添加字段的颜色
+     * @return
+     */
     private Spanned create(CharSequence content, String label, int color) {
         SpannableStringBuilder builder = new SpannableStringBuilder(label);
         builder.setSpan(new ForegroundColorSpan(color), 0, label.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         return new SpannableStringBuilder(content).append(builder);
     }
 
+    /**
+     * 展开文本，并判断最后展开提示是否需要换行
+     *
+     * @return
+     */
     private CharSequence createContent() {
         //必须先设置text才能进行修改
-        setTextInternal(mText);
+        setTextInternal(fixText);
         if (mLessText == null || mLessText.length() == 0) {
-            return mText;
+            return fixText;
         }
         Layout layout = getLayout();
         //获得textView的最后一行
         int start = layout.getLineStart(getLineCount() - 1);
         //把最后一行和点击的文字加起来判断是否超出。
-        String content = mText.toString().substring(start, mText.length()) + mLessText;
+        String content = fixText.toString().substring(start, fixText.length()) + mLessText;
         int len = getPaint().breakText(content, 0, content.length(), true, layout.getWidth(), null);
         //len小于content的大小的话说明要换行
-        if (len < content.length()){
-            content = content.substring(len,content.length());
+        if (len < content.length()) {
+            content = content.substring(len, content.length());
         }
         //填充的字符，如果是空格的话在开头会被textView自动删除所以赋值其他字符
-        space = mLessText.substring(0,1);
+        space = mLessText.substring(0, 1);
         String moreSpace = "";
         float width = 0;
         //判断是否填满了一行
-        while (width < layout.getWidth()){
+        while (width < layout.getWidth()) {
             content = content + space;
             width = getPaint().measureText(content, 0, content.length());
             if (width < layout.getWidth()) {
@@ -162,22 +162,27 @@ public class ReadMoreTextView extends TextView {
             }
         }
         //将填充的字符设为透明
-        Spanned fill = create(mText, moreSpace, android.R.color.transparent);
+        Spanned fill = create(fixText, moreSpace, android.R.color.transparent);
         return create(fill, mLessText, mLessColor);
     }
 
+    /**
+     * 创建收起后的文本并填充有换行符的行
+     *
+     * @return
+     */
     private CharSequence createSummary() {
         if (mMoreText == null || mMoreText.length() == 0) {
-            return mText;
+            return fixText;
         }
         Layout layout = getLayout();
         //返回指定的文本偏移的开始
         int start = layout.getLineStart(mMaxLines - 1);
         //返回文本抵消在指定行最后一个字符之后。
-        int end = layout.getLineEnd(mMaxLines - 1)  - start;
+        int end = layout.getLineEnd(mMaxLines - 1) - start;
 
         //返回一个新CharSequence进行这个序列的子序列。
-        CharSequence content = mText.subSequence(start, mText.length());
+        CharSequence content = fixText.subSequence(start, start + end);
         CharSequence extra = ellipsis + mMoreText;
 
         float moreWidth = getPaint().measureText(extra, 0, extra.length());
@@ -187,10 +192,59 @@ public class ReadMoreTextView extends TextView {
         if (content.charAt(end - 1) == '\n') {
             end = end - 1;
         }
+        content = fixText.subSequence(start, start + len);
+        String moreSpace = "";
+        float width = 0;
+        //判断是否填满了一行
+        while (width < maxWidth) {
+            content = content + space;
+            width = getPaint().measureText(content, 0, content.length());
+            if (width < maxWidth) {
+                moreSpace = moreSpace + space;
+            }
+        }
         len = Math.min(len, end);
-        return create(mText.subSequence(0, start + len) + ellipsis, mMoreText, mMoreColor);
+        //将填充的字符设为透明
+        Spanned fill = create(fixText.subSequence(0, start + len) + ellipsis, moreSpace, android.R.color.transparent);
+        return create(fill, mMoreText, mMoreColor);
     }
 
+    /**
+     * 整理中英文混合排序提前换行的问题
+     *
+     * @param line
+     */
+    private void fixTextWithEnter(int line) {
+        if (fixText == null) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder(fixText);
+        int textLine = getLineCount();
+        Layout layout = getLayout();
+        int start = layout.getLineStart(line);
+        int end = layout.getLineEnd(line);
+        try {
+            String content = fixText.substring(start, end);
+            int len = getPaint().breakText(fixText, start, fixText.length(), true, layout.getWidth(), null);
+            //判断如果提前换行了则加换行符进行截断
+            if (content.contains("\n") || len == end - start) {
+            } else {
+                sb.insert(start + len, "\n");
+                fixText = sb.toString();
+                setTextInternal(fixText);
+            }
+        } catch (Exception e) {
+        }
+        if (line + 1 < textLine) {
+            fixTextWithEnter(line + 1);
+        }
+    }
+
+    /**
+     * 设置文本，不用自己的setText不然getText会改变了
+     *
+     * @param text
+     */
     private void setTextInternal(CharSequence text) {
         super.setText(text, mBufferType);
     }
@@ -207,7 +261,7 @@ public class ReadMoreTextView extends TextView {
 
         @Override
         public void onClick(View view) {
-            if (expand) {
+            if (!expand) {
                 if (content == null) {
                     content = createContent();
                 }
